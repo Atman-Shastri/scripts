3b83a65ef6854e06a204d3982d053ab659f54894
Atman Shastri <126988436+Atman-Shastri@users.noreply.github.com>
2024-01-23 05:55:38 +0530

    Update setup_py.sh

--- setup_py.sh
+++ setup_py.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-echo | sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt update
-sudo apt install python3.12 -y
-echo 'alias py="python3.12"' >> ~/.bashrc
-curl -sSL https://bootstrap.pypa.io/get-pip.py -o ~/get-pip.py
-python3.12 ~/get-pip.py "pip==23.3.2"
-python3.12 -m pip install poetry
-echo 'alias poetry="python3.12 -m poetry"' >> ~/.bashrc
+#echo | sudo add-apt-repository ppa:deadsnakes/ppa
+#sudo apt update
+#sudo apt install python3.12 -y
+#echo 'alias py="python3.12"' >> ~/.bashrc
+#curl -sSL https://bootstrap.pypa.io/get-pip.py -o ~/get-pip.py
+#python3.12 ~/get-pip.py "pip==23.3.2"
+#python3.12 -m pip install poetry
+#echo 'alias poetry="python3.12 -m poetry"' >> ~/.bashrc
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install python3-pip
+python3 -m pip install poetry
+echo 'alias poetry="python3 -m poetry"' >> ~/.bashrc
+echo 'alias pip="python3 -m pip"' >> ~/.bashrc
+source ~/.bashrc
